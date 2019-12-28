9f0fcd73c8feb94ef8a54c373c55984eb7b0102c
Catherine Pedersen <cathy.pedersen@gmail.com>
2019-12-27 20:49:29 -0500

    Implemented fetch assignment 1

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImages(num) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
     .then(responseJson => 
-      displayResults(responseJson))
+      printResults(responseJson, num))
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayResults(responseJson) {
-  console.log(responseJson);
+function printResults(responseJson, num) {
   //replace the existing image with the new one
-  $('.results-img').replaceWith(
+  console.log(`${num} random dogs selected`);
+  
+  const keys = Object.keys(responseJson);
+  for (const key of keys) {
+    console.log(responseJson[key]);
+  }
+
+  /*for (let item in responseJson) {
+    console.log(`${responseJson.message}`);
+  }*/
+
+  /*$('.results-img').replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
-  )
+  )*/
+
   //display the results section
   $('.results').removeClass('hidden');
 }
@@ -21,7 +32,8 @@ function displayResults(responseJson) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    let num_dogs = $("#how-many").val();
+    getDogImages(num_dogs);
   });
 }
 
